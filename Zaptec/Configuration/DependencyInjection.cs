8d10269e4d57460e70fc15f4368dfdbc68ee1b61
Kári Rafn Karlsson <karirafn@gmail.com>
2022-11-13 17:49:22 +0000

    Moved Zaptec DI configuration to Zaptec project

---
+++ Zaptec/Configuration/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Zaptec.Configuration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddZaptec(this IServiceCollection services) => services
+        .AddZaptecHttpClient()
+        .AddZaptecController();
+}
---
+++ Zaptec/Configuration/HttpClientConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
+
+namespace Zaptec.Configuration;
+
+internal static class HttpClientConfiguration
+{
+    internal static IServiceCollection AddZaptecHttpClient(this IServiceCollection services) => services
+        .AddHttpClient(nameof(Zaptec), options =>
+        {
+            options.BaseAddress = new Uri("https://api.zaptec.com/");
+
+            options.DefaultRequestHeaders.Accept.Clear();
+            options.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }).Services;
+}
